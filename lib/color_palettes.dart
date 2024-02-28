e133bbd9f5db0aa1a709eb3e066ff5edcde94630
Andrei Mishchanka <52292591+AndreiMishchanka@users.noreply.github.com>
2024-02-28 12:54:05 +0100

    add project

--- lib/color_palettes.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:ui';
-
-const mainBackgroundColor = Color(0xff1e1e1e);
-const appBarColor = Color(0xff222222);
-const explorerBackgroundColor = Color(0xff252526);
-const explorerItemsListBackgroundColor = Color(0xff1d1d1d);
--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:visual_studio_for_android/widget_factory.dart';
-import 'app.dart';
-
-void main() {
-  runApp( App(widgetFactory: WidgetFactory()) );
-}
