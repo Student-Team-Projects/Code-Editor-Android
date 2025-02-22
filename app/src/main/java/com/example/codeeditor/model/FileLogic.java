7f58e4eac5564b205482ce34a292c2678f54e9b7
Zuzanna Szewczyk <zuzaszewczyk3@gmail.com>
2025-02-22 19:32:05 +0100

    Adding part of git v2

--- app/src/main/java/com/example/codeeditor/model/FileLogic.java
+++ app/src/main/java/com/example/codeeditor/model/FileLogic.java
@@ -1,10 +1,7 @@
 package com.example.codeeditor.model;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.net.Uri;
-import android.provider.MediaStore;
-
 import androidx.documentfile.provider.DocumentFile;
 
 import org.eclipse.jgit.api.Git;
@@ -16,35 +13,21 @@ import java.io.IOException;
 
 public class FileLogic {
 
-    /**
-     * Initialize a new Git repository in the directory specified by directoryUri.
-     */
+
     public static String initGitRepository(Context context, Uri directoryUri) {
         try {
-            System.out.println("Initializing Git repository...");
-
-            if (directoryUri == null) {
-                return "Error: directoryUri is NULL!";
-            }
-
-            System.out.println("Directory URI received: " + directoryUri.toString());
-
             File repoDir = getFileFromUri(context, directoryUri);
-
             if (repoDir == null) {
-                return "Could not resolve a filesystem path from directoryUri: " + directoryUri.toString();
+                return "Could not resolve a filesystem path from directoryUri.";
             }
-
-            System.out.println("Resolved repo directory: " + repoDir.getAbsolutePath());
 
             if (!repoDir.exists()) {
                 boolean created = repoDir.mkdirs();
                 if (!created) {
                     return "Unable to create directory: " + repoDir.getAbsolutePath();
+
                 }
             }
-
-            System.out.println("Creating Git repository in: " + repoDir.getAbsolutePath());
 
             Git git = Git.init()
                     .setDirectory(repoDir)
@@ -55,19 +38,13 @@ public class FileLogic {
 
         } catch (GitAPIException e) {
             e.printStackTrace();
-            return "initGitRepository GitAPIException: " + e.getMessage();
+            return "initGitRepository error: " + e.getMessage();
         } catch (Exception e) {
             e.printStackTrace();
             return "initGitRepository exception: " + e.getMessage();
         }
     }
 
-
-
-
-    /**
-     * Add the file at fileUri to the Git index.
-     */
     public static String gitAdd(Context context, Uri directoryUri, Uri fileUri) {
         try {
             File repoDir = getFileFromUri(context, directoryUri);
@@ -99,9 +76,6 @@ public class FileLogic {
         }
     }
 
-    /**
-     * Commit changes in the Git repository with a given commit message.
-     */
     public static String gitCommit(Context context, Uri directoryUri, String message) {
         try {
             File repoDir = getFileFromUri(context, directoryUri);
@@ -126,10 +100,6 @@ public class FileLogic {
         }
     }
 
-    /**
-     * Push changes to remote repository.
-     * If your remote requires credentials, you will need to set them here.
-     */
     public static String gitPush(Context context, Uri directoryUri) {
         try {
             File repoDir = getFileFromUri(context, directoryUri);
@@ -138,14 +108,6 @@ public class FileLogic {
             }
 
             Git git = Git.open(repoDir);
-
-            // If you need to specify a remote name or credentials:
-            // git.push()
-            //    .setRemote("origin")
-            //    .setCredentialsProvider(
-            //         new UsernamePasswordCredentialsProvider("username", "password")
-            //    )
-            //    .call();
 
             git.push().call();
             git.close();
@@ -161,35 +123,9 @@ public class FileLogic {
         }
     }
 
-
-
     private static File getFileFromUri(Context context, Uri uri) {
-        System.out.println("Resolving URI: " + uri.toString());
-
-        if ("file".equalsIgnoreCase(uri.getScheme())) {
-            File file = new File(uri.getPath());
-            System.out.println("Resolved file (file scheme): " + file.getAbsolutePath());
-            return file;
-        }
-
-        if ("content".equalsIgnoreCase(uri.getScheme())) {
-            String[] projection = {MediaStore.Files.FileColumns.DATA};
-            try (Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null)) {
-                if (cursor != null && cursor.moveToFirst()) {
-                    int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATA);
-                    String filePath = cursor.getString(columnIndex);
-                    System.out.println("Resolved file path (content scheme): " + filePath);
-                    return new File(filePath);
-                } else {
-                    System.out.println("Cursor was empty or invalid.");
-                }
-            }
-        }
-
-        System.out.println("Failed to resolve file from URI.");
-        return null;
+        return new File(uri.getPath());
     }
-
 
 
     private static String getRelativePath(File folder, File file) {
